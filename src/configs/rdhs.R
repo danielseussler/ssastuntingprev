01736541efc35eb671387b5d925e51672d02fad9
danielseussler <daniel.seussler@gmail.com>
2023-01-01 18:14:02 +0100

    Delete rdhs.R

--- src/configs/rdhs.R
+++
@@ -1,9 +0,0 @@
-# setup login for dhs api with rdhs package
-
-rdhs::set_rdhs_config(
-  email = Sys.getenv("email"),
-  project = Sys.getenv("project"),
-  timeout = 100,
-  verbose_download = TRUE,
-  data_frame = "data.table::as.data.table"
-)
